36270d156917c19d4f4926d89dfe97faeffa23d8
OCoudray1968 <56999331+OCoudray1968@users.noreply.github.com>
2020-07-17 08:27:31 +0200

    MAJ du 17/07 avec ajout des tests JUnit et correction des bugs
    Change-Id: I036497da27c34b7a8d13e9a5494ca1f004825271
    Signed-off-by: OCoudray1968 <56999331+OCoudray1968@users.noreply.github.com>

--- src/com/classicModels/managers/LoginsManager.java
+++ src/com/classicModels/managers/LoginsManager.java
@@ -6,75 +6,84 @@ import com.classicModels.DAO.LoginsDAO;
 import com.classicModels.DTO.AbstractDTO;
 import com.classicModels.DTO.LoginsDTO;
 
-
 public class LoginsManager {
 
-	 public void update(AbstractDTO source, AbstractDTO cible) {
+	public static void update(AbstractDTO source, AbstractDTO cible) {
 
-	    	LoginsDTO dtoSource = (LoginsDTO)source;
-	    	LoginsDTO dtoCible = (LoginsDTO)cible;
+		LoginsDTO dtoSource = (LoginsDTO) source;
+		LoginsDTO dtoCible = (LoginsDTO) cible;
 
+		System.out.println("source :" + dtoSource);
+		System.out.println("cible :" + dtoCible);
+
+		if (dtoCible.getCustomerNumber() == 0)
 			dtoCible.setCustomerNumber(dtoSource.getCustomerNumber());
-			
-			if (dtoCible.getEmployeeNumber()==0)
-			    dtoCible.setEmployeeNumber(dtoSource.getEmployeeNumber());
-			
-			if (dtoCible.getLogin().isEmpty())
-			    dtoCible.setLogin(dtoSource.getLogin());
-		
-			if (dtoCible.getPasswordLogin().isEmpty())
-			    dtoCible.setPasswordLogin(dtoSource.getPasswordLogin());
-			
-			if (dtoCible.getProfil()==0)
-				dtoCible.setProfil(dtoSource.getProfil());
-			
-					
 
-			LoginsDAO login = new LoginsDAO();	
-			login.update(dtoCible);
-	    }
+		if (dtoCible.getEmployeeNumber() == 0)
+			dtoCible.setEmployeeNumber(dtoSource.getEmployeeNumber());
 
-	    /**
-	     * Retourne une liste de DTOs correspondant � la liste des enregistrements pr�sent dans la table <code>classicmodels.Customers</code>.
-	     * @see com.bd.manager.InterfaceManager#listAll(Object bean)
-	     */
-	  /**
-	     * Retourne une liste de DTOs correspondant à la liste des enregistrements présent dans la table <code>classicmodels.Customers</code>.
-	     * 
-	     */
-	    public static List<AbstractDTO> listAll() {
-	    	LoginsDAO login = new LoginsDAO();
-	    	return login.listAll(new LoginsDTO());
-	    }
+		if (dtoCible.getLogin().isEmpty())
+			dtoCible.setLogin(dtoSource.getLogin());
 
+		if (dtoCible.getPasswordLogin().isEmpty())
+			dtoCible.setPasswordLogin(dtoSource.getPasswordLogin());
 
-		/**
-		 * Retourne un seul enregistrement
-		 * @param bean
-		 * @return
-		 */
-		public static AbstractDTO getRecord(AbstractDTO bean) {
-			LoginsDAO login = new LoginsDAO(); 
-			List <AbstractDTO>l = login.listAll(bean);
-			try {
-				return l.get(0);
-			} catch (Exception e) {
-			
-				return null;
-			}
-			
-				
-			
+		if (dtoCible.getProfil() == 0)
+			dtoCible.setProfil(dtoSource.getProfil());
+
+		LoginsDAO login = new LoginsDAO();
+		login.update(dtoCible);
+	}
+
+	/**
+	 * Retourne une liste de DTOs correspondant � la liste des enregistrements
+	 * pr�sent dans la table <code>classicmodels.Customers</code>.
+	 * 
+	 * @see com.bd.manager.InterfaceManager#listAll(Object bean)
+	 */
+	/**
+	 * Retourne une liste de DTOs correspondant à la liste des enregistrements
+	 * présent dans la table <code>classicmodels.Customers</code>.
+	 * 
+	 */
+	public static List<AbstractDTO> listAll() {
+		LoginsDAO login = new LoginsDAO();
+		return login.listAll(new LoginsDTO());
+	}
+
+	/**
+	 * Retourne un seul enregistrement
+	 * 
+	 * @param bean
+	 * @return
+	 */
+	public static AbstractDTO getRecord(AbstractDTO bean) {
+		LoginsDAO login = new LoginsDAO();
+		List<AbstractDTO> l = login.listAll(bean);
+		System.out.println(l);
+		try {
+			return l.get(0);
+		} catch (Exception e) {
+
+			return null;
 		}
-		
-		/**
-		 * Insère un enregistrement
-		 * @param bean
-		 */
-		public static void setRecord(AbstractDTO bean) {
-			LoginsDAO login = new LoginsDAO();
-			login.create(bean);
-		}
-		
+
+	}
+
+	/**
+	 * Insère un enregistrement
+	 * 
+	 * @param bean
+	 */
+	public static void setRecord(AbstractDTO bean) {
+		LoginsDAO login = new LoginsDAO();
+		login.create(bean);
+	}
+
+	public static void delRecord(AbstractDTO bean) {
+		LoginsDAO login = new LoginsDAO();
+		login.delete(bean);
+
+	}
 
 }
---
+++ src/com/classicModels/tests/OfficeDAOTest.java
@@ -0,0 +1,70 @@
+package com.classicModels.tests;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import com.classicModels.DTO.OfficesDTO;
+import com.classicModels.managers.ManagerFactory;
+import com.classicModels.managers.OfficesManager;
+
+class OfficeDAOTest {
+
+	@Test
+	public void createTest() {
+
+		OfficesDTO c = ManagerFactory.getOffices();
+
+		// Creation d'un client
+		c.setOfficeCode(8);
+		;
+		c.setCity("Nice");
+		c.setPhoneOffice("+33 4 90 12 27 68");
+		c.setAddressLine1("244 route de turin");
+		c.setAddressLine2("");
+		c.setStateOffice("");
+		c.setCountryOffice("France");
+		c.setPostalCode("06160");
+		c.setTerritory("EMEA");
+		OfficesManager.setRecord(c);
+		long id = c.getOfficeCode();
+		Assert.assertNotNull(id);
+		OfficesDTO office = (OfficesDTO) OfficesManager.getRecord(c);
+		// Recherche d'un client
+
+		Assert.assertEquals(8, office.getOfficeCode());
+		Assert.assertEquals("Nice", office.getCity());
+		return;
+	}
+
+	@Test
+	public void updateTest() {
+
+		// MAJ d'un client
+		OfficesDTO c = ManagerFactory.getOffices();
+		c.setOfficeCode(0);
+		;
+		OfficesDTO office = (OfficesDTO) OfficesManager.getRecord(c);
+		System.out.println(office);
+		OfficesDTO c2 = ManagerFactory.getOffices();
+		c2.setAddressLine2("bat A");
+		c2.setStateOffice("Alpes Maritime");
+		OfficesManager.update(office, c2);
+		OfficesDTO offices = (OfficesDTO) OfficesManager.getRecord(c);
+		Assert.assertEquals("bat A", offices.getAddressLine2());
+		Assert.assertEquals("Alpes Maritime", offices.getStateOffice());
+
+		return;
+	}
+
+	@Test
+	public void deleteTest() {
+
+		OfficesDTO c = ManagerFactory.getOffices();
+		c.setOfficeCode(8);
+		OfficesManager.delRecord(c);
+
+		return;
+
+	}
+
+}
